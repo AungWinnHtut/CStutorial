0086f69fc56f1f520031410d07d589cc045f379b
Dr. Aung Win Htut <aungwinnhtut@gmail.com>
2022-01-15 21:09:55 +0630

    image viewer - password char

---
+++ ImageViewer/ImageViewer/frmPhoto.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace ImageViewer
+{
+    partial class frmPhoto
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmPhoto));
+            this.picPhoto = new System.Windows.Forms.PictureBox();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.btn3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.picPhoto)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picPhoto
+            // 
+            this.picPhoto.Image = ((System.Drawing.Image)(resources.GetObject("picPhoto.Image")));
+            this.picPhoto.Location = new System.Drawing.Point(31, 24);
+            this.picPhoto.Name = "picPhoto";
+            this.picPhoto.Size = new System.Drawing.Size(599, 498);
+            this.picPhoto.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picPhoto.TabIndex = 0;
+            this.picPhoto.TabStop = false;
+            // 
+            // btn1
+            // 
+            this.btn1.AccessibleRole = System.Windows.Forms.AccessibleRole.TitleBar;
+            this.btn1.Location = new System.Drawing.Point(681, 186);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(187, 56);
+            this.btn1.TabIndex = 1;
+            this.btn1.Text = "One";
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // btn2
+            // 
+            this.btn2.Location = new System.Drawing.Point(681, 266);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(187, 56);
+            this.btn2.TabIndex = 2;
+            this.btn2.Text = "Two";
+            this.btn2.UseVisualStyleBackColor = true;
+            this.btn2.Click += new System.EventHandler(this.btn2_Click);
+            // 
+            // btn3
+            // 
+            this.btn3.Location = new System.Drawing.Point(681, 342);
+            this.btn3.Name = "btn3";
+            this.btn3.Size = new System.Drawing.Size(187, 56);
+            this.btn3.TabIndex = 3;
+            this.btn3.Text = "Three";
+            this.btn3.UseVisualStyleBackColor = true;
+            this.btn3.Click += new System.EventHandler(this.btn3_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(690, 97);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(78, 20);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Password";
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(791, 91);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(115, 26);
+            this.txtPassword.TabIndex = 5;
+            this.txtPassword.TextChanged += new System.EventHandler(this.txtPassword_TextChanged);
+            this.txtPassword.KeyUp += new System.Windows.Forms.KeyEventHandler(this.txtPassword_KeyUp);
+            // 
+            // frmPhoto
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1091, 549);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btn3);
+            this.Controls.Add(this.btn2);
+            this.Controls.Add(this.btn1);
+            this.Controls.Add(this.picPhoto);
+            this.Name = "frmPhoto";
+            this.Text = "Photo Viewer";
+            this.Load += new System.EventHandler(this.frmPhoto_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.picPhoto)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picPhoto;
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Button btn3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtPassword;
+    }
+}
+
---
+++ ImageViewer/ImageViewer/frmPhoto.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ImageViewer
+{
+    public partial class frmPhoto : Form
+    {
+        string sPassword;
+        char[] caPass = new char[10];
+        int iCounter = 0;
+        bool isEnter = false;
+
+        public frmPhoto()
+        {
+            InitializeComponent();
+        }
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            picPhoto.Image = Image.FromFile("1.jpg");
+        }
+
+        private void btn2_Click(object sender, EventArgs e)
+        {
+            picPhoto.Image = Image.FromFile("2.jpg");
+        }
+
+        private void btn3_Click(object sender, EventArgs e)
+        {
+           
+
+
+            if(isEnter)
+            {
+                string sFinalPass =new string(caPass);
+                sFinalPass = sFinalPass.Substring(0, iCounter );
+                if (sFinalPass  == "1234")
+                {
+                    picPhoto.Image = Image.FromFile("3.png");
+                }
+            }
+            else
+            {
+                picPhoto.Image = Image.FromFile("1.jpg");
+            }
+            
+        }
+
+     
+
+       
+
+        private void txtPassword_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyData == Keys.Enter)
+            {
+                isEnter = true;
+                btn3_Click(sender,e);
+            }
+            else
+            {
+                string sBuffer;
+                char cTemp;
+                string sStar = "";
+                iCounter = iCounter + 1; // iCounter++;
+                sBuffer = txtPassword.Text;
+                cTemp = sBuffer.ElementAt(iCounter - 1);
+                caPass[iCounter - 1] = cTemp;
+                for (int i = 0; i < iCounter; i++)
+                {
+                    sStar = sStar + "*";// sStar += "*";
+                }
+                txtPassword.Text = sStar;
+                txtPassword.Select(iCounter, 0);
+            }
+
+        }
+
+        private void frmPhoto_Load(object sender, EventArgs e)
+        {
+            this.ActiveControl = txtPassword;
+        }
+
+        private void txtPassword_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
