aaefa55ba808ba8a50f5d62417453f98789f2934
Dr. Aung Win Htut <aungwinnhtut@gmail.com>
2022-01-18 20:47:48 +0630

    adding binary code 1

---
+++ binaryadd/binaryadd/main.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+	//funConvertS2Bin();
+	//funAddBin(a,b);
+	//funConvertBin2String();
+
+	return 0;
+}
+
+
+funConvertS2Bin(String );
+//funAddBin(a,b);
+//funConvertBin2String();
