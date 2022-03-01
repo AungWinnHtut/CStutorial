fd4cc1170c08a5579b85b49f645a5c2c8bc3d179
Dr. Aung Win Htut <aungwinnhtut@gmail.com>
2022-03-01 18:58:34 +0630

    Multiple Choice
    Multiple Choice

--- Cprog/2022022401 CS Login Exam 2/2022022401 CS Login Exam 2/frmPhysics.Designer.cs
+++ Cprog/2022022401 CS Login Exam 2/2022022401 CS Login Exam 2/frmPhysics.Designer.cs
@@ -28,12 +28,128 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.txtPhysicsQuestion = new System.Windows.Forms.RichTextBox();
+            this.lblAns1 = new System.Windows.Forms.Label();
+            this.lblAns2 = new System.Windows.Forms.Label();
+            this.lblAns3 = new System.Windows.Forms.Label();
+            this.txtMultipleChoice = new System.Windows.Forms.TextBox();
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.txtPhysicsTotalMarks = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnNextPhysicsQuestion = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtPhysicsQuestion
+            // 
+            this.txtPhysicsQuestion.Enabled = false;
+            this.txtPhysicsQuestion.Location = new System.Drawing.Point(18, 55);
+            this.txtPhysicsQuestion.Name = "txtPhysicsQuestion";
+            this.txtPhysicsQuestion.Size = new System.Drawing.Size(958, 86);
+            this.txtPhysicsQuestion.TabIndex = 0;
+            this.txtPhysicsQuestion.Text = "";
+            // 
+            // lblAns1
+            // 
+            this.lblAns1.AutoSize = true;
+            this.lblAns1.Location = new System.Drawing.Point(23, 171);
+            this.lblAns1.Name = "lblAns1";
+            this.lblAns1.Size = new System.Drawing.Size(51, 20);
+            this.lblAns1.TabIndex = 1;
+            this.lblAns1.Text = "label1";
+            // 
+            // lblAns2
+            // 
+            this.lblAns2.AutoSize = true;
+            this.lblAns2.Location = new System.Drawing.Point(344, 171);
+            this.lblAns2.Name = "lblAns2";
+            this.lblAns2.Size = new System.Drawing.Size(51, 20);
+            this.lblAns2.TabIndex = 2;
+            this.lblAns2.Text = "label2";
+            // 
+            // lblAns3
+            // 
+            this.lblAns3.AutoSize = true;
+            this.lblAns3.Location = new System.Drawing.Point(694, 171);
+            this.lblAns3.Name = "lblAns3";
+            this.lblAns3.Size = new System.Drawing.Size(51, 20);
+            this.lblAns3.TabIndex = 3;
+            this.lblAns3.Text = "label3";
+            // 
+            // txtMultipleChoice
+            // 
+            this.txtMultipleChoice.Location = new System.Drawing.Point(27, 224);
+            this.txtMultipleChoice.Name = "txtMultipleChoice";
+            this.txtMultipleChoice.Size = new System.Drawing.Size(103, 26);
+            this.txtMultipleChoice.TabIndex = 4;
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.Location = new System.Drawing.Point(169, 224);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(807, 41);
+            this.btnSubmit.TabIndex = 5;
+            this.btnSubmit.Text = "Submit";
+            this.btnSubmit.UseVisualStyleBackColor = true;
+            this.btnSubmit.Click += new System.EventHandler(this.btnSubmit_Click);
+            // 
+            // txtPhysicsTotalMarks
+            // 
+            this.txtPhysicsTotalMarks.Location = new System.Drawing.Point(855, 13);
+            this.txtPhysicsTotalMarks.Name = "txtPhysicsTotalMarks";
+            this.txtPhysicsTotalMarks.Size = new System.Drawing.Size(120, 26);
+            this.txtPhysicsTotalMarks.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(744, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(91, 20);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Total Marks";
+            // 
+            // btnNextPhysicsQuestion
+            // 
+            this.btnNextPhysicsQuestion.Location = new System.Drawing.Point(829, 271);
+            this.btnNextPhysicsQuestion.Name = "btnNextPhysicsQuestion";
+            this.btnNextPhysicsQuestion.Size = new System.Drawing.Size(146, 30);
+            this.btnNextPhysicsQuestion.TabIndex = 8;
+            this.btnNextPhysicsQuestion.Text = ">>>>>";
+            this.btnNextPhysicsQuestion.UseVisualStyleBackColor = true;
+            this.btnNextPhysicsQuestion.Click += new System.EventHandler(this.btnNextPhysicsQuestion_Click);
+            // 
+            // frmPhysics
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(995, 313);
+            this.Controls.Add(this.btnNextPhysicsQuestion);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtPhysicsTotalMarks);
+            this.Controls.Add(this.btnSubmit);
+            this.Controls.Add(this.txtMultipleChoice);
+            this.Controls.Add(this.lblAns3);
+            this.Controls.Add(this.lblAns2);
+            this.Controls.Add(this.lblAns1);
+            this.Controls.Add(this.txtPhysicsQuestion);
+            this.Name = "frmPhysics";
             this.Text = "frmPhysics";
+            this.Load += new System.EventHandler(this.frmPhysics_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.RichTextBox txtPhysicsQuestion;
+        private System.Windows.Forms.Label lblAns1;
+        private System.Windows.Forms.Label lblAns2;
+        private System.Windows.Forms.Label lblAns3;
+        private System.Windows.Forms.TextBox txtMultipleChoice;
+        private System.Windows.Forms.Button btnSubmit;
+        private System.Windows.Forms.TextBox txtPhysicsTotalMarks;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnNextPhysicsQuestion;
     }
 }
--- Cprog/2022022401 CS Login Exam 2/2022022401 CS Login Exam 2/frmPhysics.cs
+++ Cprog/2022022401 CS Login Exam 2/2022022401 CS Login Exam 2/frmPhysics.cs
@@ -7,14 +7,63 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO; /// <summary>
+/// 
+/// </summary>
 
 namespace _2022022401_CS_Login_Exam_2
 {
     public partial class frmPhysics : Form
     {
+        int iPhysicsPages = 0;
+        int iPhysicTotalMarks = 0;
         public frmPhysics()
         {
             InitializeComponent();
+        }
+
+        private void frmPhysics_Load(object sender, EventArgs e)
+        {
+            txtPhysicsQuestion.Text = "Speed of light is ......";
+            lblAns1.Text = "(a) 299 792 458 m/s";
+            lblAns2.Text = "(b) 499 792 458 m/s";
+            lblAns3.Text = "(c) 999 792 458 m/s";
+
+
+
+        }
+
+        private void btnSubmit_Click(object sender, EventArgs e)
+        {
+            
+            string sAns = txtMultipleChoice .Text;
+            if(sAns == "a")
+            {
+                iPhysicTotalMarks += 10;
+                MessageBox.Show("Correct Answer!");
+                txtPhysicsTotalMarks.Text = iPhysicTotalMarks.ToString();
+            }
+            else
+            {
+                MessageBox.Show("Wrong Answer!");
+            }
+        }
+
+        private void btnNextPhysicsQuestion_Click(object sender, EventArgs e)
+        {
+            iPhysicsPages++;
+            string sPhysicsQuestion = "";
+
+
+            StreamReader srPhysicsQuestion = new StreamReader("physics_questions.txt");
+            sPhysicsQuestion = srPhysicsQuestion.ReadToEnd();
+
+            txtPhysicsQuestion.Text = sPhysicsQuestion ;
+            lblAns1.Text = "";
+            lblAns2.Text = "";
+            lblAns3.Text = "";
+            txtMultipleChoice.Clear();
+            txtMultipleChoice.Focus();
         }
     }
 }
