59e81073eb2c06f91291e7896f5640a270fa8a1c
Dr. Aung Win Htut <aungwinnhtut@gmail.com>
2022-03-02 19:08:50 +0630

    Read Line by Line and Split
    Read Line by Line and Split

---
+++ Cprog/2022030201 CS Array Tutorial/2022030201 CS Array Tutorial/frmArray.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace _2022030201_CS_Array_Tutorial
+{
+    partial class frmArray
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnTest = new System.Windows.Forms.Button();
+            this.txtAnswer = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // btnTest
+            // 
+            this.btnTest.Location = new System.Drawing.Point(60, 320);
+            this.btnTest.Name = "btnTest";
+            this.btnTest.Size = new System.Drawing.Size(169, 36);
+            this.btnTest.TabIndex = 0;
+            this.btnTest.Text = "Test";
+            this.btnTest.UseVisualStyleBackColor = true;
+            this.btnTest.Click += new System.EventHandler(this.btnTest_Click);
+            // 
+            // txtAnswer
+            // 
+            this.txtAnswer.Location = new System.Drawing.Point(44, 32);
+            this.txtAnswer.Name = "txtAnswer";
+            this.txtAnswer.Size = new System.Drawing.Size(561, 268);
+            this.txtAnswer.TabIndex = 1;
+            this.txtAnswer.Text = "";
+            // 
+            // frmArray
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtAnswer);
+            this.Controls.Add(this.btnTest);
+            this.Name = "frmArray";
+            this.Text = "Array Tutorials";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnTest;
+        private System.Windows.Forms.RichTextBox txtAnswer;
+    }
+}
+
---
+++ Cprog/2022030201 CS Array Tutorial/2022030201 CS Array Tutorial/frmArray.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace _2022030201_CS_Array_Tutorial
+{
+    public partial class frmArray : Form
+    {
+        public frmArray()
+        {
+            InitializeComponent();
+        }
+
+        private void btnTest_Click(object sender, EventArgs e)
+        {
+            // File ကနေ တစ်ကြောင်းချင်းဖတ်ရန်
+            string line = "";
+            string[] saPQ = new string[6];
+            StreamReader sr = new StreamReader("questions.txt");
+            // တစ်လိုင်းချင်းဖတ်မှာ
+            while ((line = sr.ReadLine()) != null)
+            {
+                // တစ်ကြောင်းချင်းကနေ အပိုင်း၆ပိုင်း ခွဲထုတ်ရန်               
+                saPQ = line.Split('$');
+                // အပိုင်း ၆ ပိုင်းကို 5 ပိုင်းကို UI ပေါ်တင်ရမယ်။ User အဖြေကို နောက်ဆုံး အပိုင်း အဖြေမှန်နဲ့ တိုက်ဆိုင်စစ်ဆေးမယ်
+            }
+            sr.Close();
+
+           
+            
+            
+        }
+    }
+}
