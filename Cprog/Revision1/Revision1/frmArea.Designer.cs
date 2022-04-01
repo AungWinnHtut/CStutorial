cea197691c5cd2fa33b3604c98cda99fad3d7bc2
Dr. Aung Win Htut <aungwinnhtut@gmail.com>
2022-04-01 19:01:59 +0630

    AreaOfCircle
    AreaOfCircle

---
+++ Cprog/Revision1/Revision1/frmArea.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace Revision1
+{
+    partial class frmArea
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtR = new System.Windows.Forms.TextBox();
+            this.txtA = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtR
+            // 
+            this.txtR.Location = new System.Drawing.Point(140, 30);
+            this.txtR.Name = "txtR";
+            this.txtR.Size = new System.Drawing.Size(209, 26);
+            this.txtR.TabIndex = 0;
+            this.txtR.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtR_KeyDown);
+            // 
+            // txtA
+            // 
+            this.txtA.Enabled = false;
+            this.txtA.Location = new System.Drawing.Point(140, 88);
+            this.txtA.Name = "txtA";
+            this.txtA.Size = new System.Drawing.Size(209, 26);
+            this.txtA.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(90, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Radius(cm)";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(26, 89);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(95, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Area(sq cm)";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtA);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.txtR);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(379, 145);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btnExit);
+            this.groupBox2.Controls.Add(this.btnCalculate);
+            this.groupBox2.Location = new System.Drawing.Point(422, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(159, 144);
+            this.groupBox2.TabIndex = 5;
+            this.groupBox2.TabStop = false;
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(13, 22);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(136, 44);
+            this.btnCalculate.TabIndex = 0;
+            this.btnCalculate.Text = "Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(13, 88);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(136, 44);
+            this.btnExit.TabIndex = 1;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            // 
+            // frmArea
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(616, 179);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "frmArea";
+            this.Text = "Area of circle";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtR;
+        private System.Windows.Forms.TextBox txtA;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Button btnCalculate;
+    }
+}
+
---
+++ Cprog/Revision1/Revision1/frmArea.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Revision1
+{
+    public partial class frmArea : Form
+    {
+        public frmArea()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            funCalArea();
+          
+        }
+
+        private void txtR_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Enter)
+            {
+                funCalArea();
+                txtR .SelectAll();
+                txtR.Focus();
+            }
+
+        }
+
+        public void funCalArea()
+        {
+
+            float fArea = 0.0f;
+            float fRadius = 0.0f;
+
+            // varname = value;
+            // varname
+            fRadius = float.Parse(txtR.Text);
+
+            // bool bStatus = float.TryParse (txtR .Text, out fRadius);
+            // if(bStatus)
+
+            if (float.TryParse(txtR.Text, out fRadius))
+            {
+                // data valid
+                fArea = 3.14f * fRadius * fRadius;
+                txtA.Text = fArea.ToString();
+            }
+            else
+            {
+                //data invalid
+                //show error and ask to try again
+            }
+
+
+
+            // 1. User ထည့်ပေးလိုက်တဲ့ တန်ဖိုးကို စစ်မယ်
+            // float ဖြစ်လား မဖြစ်လား စစ်ဖို့လိုမယ်
+            // float.tryparse()
+            // Validation
+
+            // Store လုပ်ဖို့ = assignment operator
+
+            // Hand Calculation
+            // Equation 
+            // fA = 3.14f * fR * fR;
+
+
+            // Outpur Results into UI
+            // Store Results
+        }
+    }
+}
